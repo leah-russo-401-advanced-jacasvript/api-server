a34f4ddcf416b48a50b8b72100b9ce6e5762aaf9
leahgrace555 <leah.grace555@gmail.com>
2020-10-19 18:13:33 -0400

    added dotenv

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 //need to get the json-server library
+require('dotenv').config();
+
 const jsonServer = require('json-server');
 
 const server = jsonServer.create();
@@ -9,11 +11,11 @@ const router = jsonServer.router('db.json');
 
 const middleware = jsonServer.defaults();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;
 
 server.use(middleware);
 server.use(router);
 
 server.listen( PORT, () => {
-  console.log('JSON server is listening')
+  console.log(`JSON server is listening on PORT ${PORT}` )
 })
